681648e65f7c9d20195f0db4bf0eb519272be4c4
KloderGit <idzhyan.develop@outlook.com>
2020-09-03 17:16:32 +0300

    add Select option for Repository

---
+++ lc.fitness-pro.library/Interface/ISelect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace lc.fitnesspro.library.Interface
+{
+    public interface ISelect<T>
+    {
+        IRepository<T> Select(Expression<Func<T, bool>> expression);
+    }
+}
