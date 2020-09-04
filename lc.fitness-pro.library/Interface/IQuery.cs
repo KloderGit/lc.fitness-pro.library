6993807a42e608cea7d3c984d357bc464e9b71d6
KloderGit <idzhyan.develop@outlook.com>
2020-09-04 19:48:06 +0300

    add Expand by Interface fields and attributes

--- lc.fitness-pro.library/Interface/IQuery.cs
+++ lc.fitness-pro.library/Interface/IQuery.cs
@@ -8,11 +8,13 @@ namespace lc.fitnesspro.library.Interface
     public interface IQuery<T>
     {        
         IQuery<T> Select(Expression<Func<T, object>> expression);
-        IQuery<T> Expand(Expression<Func<T, object>> expression);
+        //IQuery<T> Expand(Expression<Func<T, object>> expression);
+        IQuery<T> Expand(Expression<Func<IContractExpandField, object>> expression);
         IQuery<T> Filter(Expression<Predicate<T>> expression);
         IQuery<T> And();
         IQuery<T> Or();
 
         string Build();
+
     }
 }
