64e9147c6a23dfd3b45ce781a8739c997b688dcc
KomuhFpa <kloder@yandex.ru>
2026-03-18 12:36:11 +0300

    fix typization for FilterStudentsInfo

--- lc.fitness-pro.library/ContractRepository.cs
+++ lc.fitness-pro.library/ContractRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using lc.fitnesspro.library.Interface;
 using lc.fitnesspro.library.Model;
@@ -18,7 +19,60 @@ namespace lc.fitnesspro.library
             var array = GetQueryStringArray(students);
             return await GetByStudentsRequest(array);
         }
+        
+        
+        public async Task<IEnumerable<StudentEducationInfoSlim>> FilterStudentsInfo(
+            List<Guid> educationProgramKeys,
+            List<Guid> groupKeys,
+            List<Guid> subGroupKeys,
+            int? top,
+            int? skip,
+            CancellationToken cancellationToken = default)
+        {
+            var programFilter = educationProgramKeys is null || educationProgramKeys.Count == 0
+                ? " and (ПрограммаОбучения_Key ne '00000000-0000-0000-0000-000000000000')"
+                : " and (" + String.Join(" or ", educationProgramKeys.Select(x => GetItemFilter("ПрограммаОбучения_Key", x))) + ")";
+            
+            var groupFilter = groupKeys is null || groupKeys.Count == 0
+                ? " and (ГруппаСлушателя_Key ne '00000000-0000-0000-0000-000000000000')"
+                : " and (" + String.Join(" or ", groupKeys.Select(x=>GetItemFilter("ГруппаСлушателя_Key", x))) + ")";
+            
+            var subGroupFilter = subGroupKeys is null || subGroupKeys.Count == 0
+                ? ""
+                : " and (" + String.Join(" or ", subGroupKeys.Select(x=>GetItemFilter("ПодгруппаСлушателя_Key", x))) + ")";
+            
+            var topFilter = top is null
+                ? ""
+                : $"&$top={top}";
+            
+            var skipFilter = skip is null
+                ? ""
+                : $"&$skip={skip}";
+            
+            var requestStringTemplate =
+                "$format=json" +
+                "&$expand=ПрограммаОбучения,Слушатель,ГруппаСлушателя,ПодгруппаСлушателя" +
+                "&$select=ПрограммаОбучения/Ref_Key," +
+                "ПрограммаОбучения/Description," +
+                "Слушатель/Ref_Key," +
+                "Слушатель/Description," +
+                "ГруппаСлушателя/Ref_Key," +
+                "ГруппаСлушателя/Description," +
+                "ПодгруппаСлушателя/Ref_Key," +
+                "ПодгруппаСлушателя/Description" +
+                "&$filter=(DeletionMark eq false)" +
+                " and (ГрупповойДоговор eq false)"
+                + programFilter + groupFilter + subGroupFilter + topFilter + skipFilter;
+            ;
+            
+            var result = await GetByQuery<StudentEducationInfoSlim>(requestStringTemplate, cancellationToken);
 
+            return result;
+
+            string GetItemFilter(string field, Guid programKey) => $"{field} eq '{programKey}'";
+        }
+        
+        
         private IEnumerable<string> GetQueryStringArray(IEnumerable<Guid> students)
         {
             if (students == null || students.Count() == 0) return new List<string>();
--- lc.library.test/ContractRepositoryTest.cs
+++ lc.library.test/ContractRepositoryTest.cs
@@ -41,50 +41,48 @@ namespace lc.library.test
         {
             var mock = new Mock<IConnection>();
             var repository = new StudentRepository(mock.Object);
+            
+                var educationProgramKeys = new List<Guid>{Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()};
+                var groupKeys = new List<Guid>{Guid.NewGuid()};
+                List<Guid> subGroupKeys = null; // new List<Guid>();
+                int? top = 10;
+                int? skip = 5;
 
-            var request = new StudentRepository.FilterStudentInfoRequest
-            {
-                //EducationProgramKeys = new List<Guid>{Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()},
-                //GroupKeys = new List<Guid>{Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()},
-                //SubGroupKeys = new List<Guid>{Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()},
-                Top = 10,
-                Skip = 5
-            };
-
-            var programFilter = request.EducationProgramKeys.Count == 0
-                ? " and (ПрограммаОбучения_Key ne '00000000-0000-0000-0000-000000000000')"
-                : " and (" + String.Join(" or ", request.EducationProgramKeys.Select(x => GetItemFilter("ПрограммаОбучения_Key", x))) + ")";
+                var programFilter = educationProgramKeys is null || educationProgramKeys.Count == 0
+                    ? " and (ПрограммаОбучения_Key ne '00000000-0000-0000-0000-000000000000')"
+                    : " and (" + String.Join(" or ", educationProgramKeys.Select(x => GetItemFilter("ПрограммаОбучения_Key", x))) + ")";
             
-            var groupFilter = request.GroupKeys.Count == 0
-                ? " and (ГруппаСлушателя_Key ne '00000000-0000-0000-0000-000000000000')"
-                : " and (" + String.Join(" or ", request.GroupKeys.Select(x=>GetItemFilter("ГруппаСлушателя_Key", x))) + ")";
+                var groupFilter = groupKeys is null || groupKeys.Count == 0
+                    ? " and (ГруппаСлушателя_Key ne '00000000-0000-0000-0000-000000000000')"
+                    : " and (" + String.Join(" or ", groupKeys.Select(x=>GetItemFilter("ГруппаСлушателя_Key", x))) + ")";
             
-            var subGroupFilter = request.SubGroupKeys.Count == 0
-                ? ""
-                : " and" + String.Join(" or ", request.SubGroupKeys.Select(x=>GetItemFilter("ПодгруппаСлушателя_Key", x))) + ")";
+                var subGroupFilter = subGroupKeys is null || subGroupKeys.Count == 0
+                    ? ""
+                    : " and (" + String.Join(" or ", subGroupKeys.Select(x=>GetItemFilter("ПодгруппаСлушателя_Key", x))) + ")";
             
-            var topFilter = request.Top is null
-                ? ""
-                : $"&$top={request.Top}";
+                var topFilter = top is null
+                    ? ""
+                    : $"&$top={top}";
             
-            var skipFilter = request.Skip is null
-                ? ""
-                : $"&$skip={request.Skip}";
+                var skipFilter = skip is null
+                    ? ""
+                    : $"&$skip={skip}";
             
-            var requestStringTemplate =
-                "$format=json" +
-                "&$expand=ПрограммаОбучения,Слушатель,ГруппаСлушателя,ПодгруппаСлушателя" +
-                "&$select=ПрограммаОбучения/Ref_Key," +
-                "ПрограммаОбучения/Description," +
-                "Слушатель/Ref_Key," +
-                "Слушатель/Description," +
-                "ГруппаСлушателя/Ref_Key," +
-                "ГруппаСлушателя/Description," +
-                "ПодгруппаСлушателя/Ref_Key," +
-                "ПодгруппаСлушателя/Description" +
-                "&$filter=(DeletionMark eq false)" +
-                " and (ГрупповойДоговор eq false)"
-                + programFilter + groupFilter + subGroupFilter + topFilter + skipFilter;
+                var requestStringTemplate =
+                    "$format=json" +
+                    "&$expand=ПрограммаОбучения,Слушатель,ГруппаСлушателя,ПодгруппаСлушателя" +
+                    "&$select=ПрограммаОбучения/Ref_Key," +
+                    "ПрограммаОбучения/Description," +
+                    "Слушатель/Ref_Key," +
+                    "Слушатель/Description," +
+                    "ГруппаСлушателя/Ref_Key," +
+                    "ГруппаСлушателя/Description," +
+                    "ПодгруппаСлушателя/Ref_Key," +
+                    "ПодгруппаСлушателя/Description" +
+                    "&$filter=(DeletionMark eq false)" +
+                    " and (ГрупповойДоговор eq false)"
+                    + programFilter + groupFilter + subGroupFilter + topFilter + skipFilter;
+                ;
             ;
             
             string GetItemFilter(string field, Guid programKey) => $"{field} eq '{programKey}'";
