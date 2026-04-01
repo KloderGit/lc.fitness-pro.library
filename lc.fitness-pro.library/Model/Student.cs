6a998017aad893f1ea17d517089a5baed6a1e75f
KomuhFpa <kloder@yandex.ru>
2026-04-01 16:09:38 +0300

    extnd student info model

--- lc.fitness-pro.library/Model/Student.cs
+++ lc.fitness-pro.library/Model/Student.cs
@@ -27,13 +27,46 @@ namespace lc.fitnesspro.library.Model
     public class StudentEducationInfoSlim
     {
         [JsonProperty("Слушатель")]
-        public BaseSlim Student { get; set; }
+        public StudentSlim Student { get; set; }
         [JsonProperty("ПрограммаОбучения")]
-        public BaseSlim EducationProgram { get; set; }
+        public EducationProgramSlim EducationProgram { get; set; }
         [JsonProperty("ГруппаСлушателя")]
-        public BaseSlim Group { get; set; }
+        public GroupSlim Group { get; set; }
         [JsonProperty("ПодгруппаСлушателя")]
         public BaseSlim SubGroup { get; set; }
+        
+        public class StudentSlim
+        {
+            [JsonProperty("Ref_Key")]
+            public Guid Key { get; set; } 
+            [JsonProperty("ФизЛицо_Key")]
+            public Guid PersonKey { get; set; }
+            [JsonProperty("Description")]
+            public string Title { get; set; }
+        }
+        
+        public class EducationProgramSlim
+        {
+            [JsonProperty("Ref_Key")]
+            public Guid Key { get; set; } 
+            [JsonProperty("ФормаОбучения_Key")]
+            public Guid EducationFormKey { get; set; }
+            [JsonProperty("Description")]
+            public string Title { get; set; }
+        }
+        
+        public class GroupSlim
+        {
+            [JsonProperty("Ref_Key")]
+            public Guid Key { get; set; } 
+            [JsonProperty("Description")]
+            public string Title { get; set; }
+            
+            [JsonProperty("ДатаНачалаОбучения")]
+            public DateTime StartDate { get; set; }
+            [JsonProperty("ДатаокончанияОбучения")]
+            public DateTime FinishDate { get; set; }
+        }
         
         public class BaseSlim
         {
